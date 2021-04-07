6610097c636bdca699d800d01cdf6cec269cf9db
Bhagyesh <bhagyeshganatra@gmail.com>
2021-04-07 19:40:18 +0530

    Fix: Data now called in sorted fashion

--- routes/indexSymbol.js
+++ routes/indexSymbol.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const Models = require('../Models')
 
 router.route('/:index').get((req,res) => {
-    Models.Index.find({"Symbol" : req.params.index})
+    Models.Index.aggregate([
+        { $match : { Symbol: req.params.index}},
+        { $sort: { Date : 1}}
+    ])
     .then((index) => res.json(index))   
     .catch(err => res.status(400).json('ERROR: '+err));
 });
--- routes/stock.js
+++ routes/stock.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const Models = require('../Models')
 
 router.route('/:stock').get((req,res) => {
-    Models.Stock.find({"Symbol" : req.params.stock})
+    Models.Stock.aggregate([
+        { $match : { Symbol: req.params.stock}},
+        { $sort: { Date : 1}}
+    ])
     .then((stock) => res.json(stock))
     .catch(err => res.status(400).json('ERROR: '+err));
 });
